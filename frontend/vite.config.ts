218e6c7469249825f6e76e41dc9f707b4e275167
T.U <takayo.uenter36@icloud.com>
2025-10-27 01:23:36 +0900

    コンポーズ設定修正

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,32 +1,38 @@
-import { defineConfig, loadEnv } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig(({ mode }) => {
   // 環境変数を読み込む
-  const env = loadEnv(mode, process.cwd(), '')
-  
+  const env = loadEnv(mode, process.cwd(), "");
+
   return {
     plugins: [vue()],
     server: {
+      host: true,
       proxy: {
-        '/api': {
+        "/api": {
           target: env.VITE_API_TARGET_URL,
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
           secure: false,
           configure: (proxy, _options) => {
-            proxy.on('error', (err, _req, _res) => {
-              console.log('proxy error', err);
+            proxy.on("error", (err, _req, _res) => {
+              console.log("proxy error", err);
             });
-            proxy.on('proxyReq', (proxyReq, req, _res) => {
-              console.log('Sending Request:', req.method, req.url, ' => ', proxyReq.path);
+            proxy.on("proxyReq", (proxyReq, req, _res) => {
+              console.log(
+                "Sending Request:",
+                req.method,
+                req.url,
+                " => ",
+                proxyReq.path
+              );
             });
-            proxy.on('proxyRes', (proxyRes, req, _res) => {
-              console.log('Received Response:', proxyRes.statusCode, req.url);
+            proxy.on("proxyRes", (proxyRes, req, _res) => {
+              console.log("Received Response:", proxyRes.statusCode, req.url);
             });
           },
-        }
-      }
-    }
-  }
-})
+        },
+      },
+    },
+  };
+});
